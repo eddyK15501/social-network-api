04e5399c284b752eab390502a16d844d5d34922d
Edward Kim <itgma1231@gmail.com>
2023-09-30 13:45:30 -0400

    userSchema defined; user model initialized

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // CommonJS import statements
 const express = require('express');
 const db = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = 3001;
 const app = express();
 
-// Middleware for parsing JSON requests/responses
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 // Connect to MongoDB, before running the Express server
 db.once('open', () => {
