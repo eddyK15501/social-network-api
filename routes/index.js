f34d67c489a4190e3405c9a05e39c28603e7aac9
Edward Kim <itgma1231@gmail.com>
2023-09-30 15:03:05 -0400

    initial router setup for controllers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // Send 404 for wrong routes
 router.use((req, res) => {
