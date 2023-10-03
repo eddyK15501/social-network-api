fbed3bb33c1985703599c4bd939a38178d0a58d1
Edward Kim <itgma1231@gmail.com>
2023-10-03 10:32:23 -0400

    deleteThought controller/route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,13 +4,14 @@ const {
   getSingleThought,
   createThought,
   updateThought,
+  deleteThought,
 } = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought).put(updateThought);
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 
